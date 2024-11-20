d0005fbf9dac3334e24a277e1a5e507cdad40d79
Ibrangrd <ibrangrd@gmail.com>
2024-11-20 14:07:48 +0530

    add new addres

--- src/pages/Scan/CusAfter.jsx
+++ src/pages/Scan/CusAfter.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const CusAfter = () => {
  return (
     <> 
-    <div className="p-20 text-2xl sm:text-3xl text-center text-red-600 bg-yellow-50 "> COMING SOON........</div>
+    <div className="p-20 text-2xl sm:text-3xl text-center text-red-600 bg-yellow-50 ">शादी के बाद के सभी खास पलों को यहाँ पाएं।</div>
     </>
 
  );
