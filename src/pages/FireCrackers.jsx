53559763b499a7ccb622703c19caf0433f12b3e1
Ibrangrd <ibrangrd@gmail.com>
2024-12-05 00:13:34 +0530

    Make firecrakers page responsive

--- src/pages/FireCrackers.jsx
+++ src/pages/FireCrackers.jsx
@@ -12,59 +12,59 @@ const FireCrakers = () => {
   return (
     <>
       <div className="bg-[#f8edeb]">
-        <div className="flex justify-evenly p-4 pt-0 pb-0 gap-10 bg-black">
+        <div className="flex justify-evenly sm:p-4 pt-0 pb-0 pl-0 sm:gap-10 gap-2 bg-black">
           <div className="flex flex-col justify-center gap-10">
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://pune.news/wp-content/uploads/2023/10/Firecrackers.webp" />
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://static.toiimg.com/photo/msid-114651439,width-96,height-65.cms" />
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://t4.ftcdn.net/jpg/05/57/64/85/360_F_557648557_hyn34muXj7kwad5cOAoOjP2lRxMZOYBl.jpg" />
+            <img className="bg-cover hidden sm:block sm:w-28 sm:h-28 rounded-3xl" src="https://pune.news/wp-content/uploads/2023/10/Firecrackers.webp" />
+            <img className="bg-cover hidden sm:block  sm:w-28 sm:h-28 rounded-3xl" src="https://static.toiimg.com/photo/msid-114651439,width-96,height-65.cms" />
+            <img className="bg-cover hidden sm:block sm:w-28 sm:h-28 rounded-3xl" src="https://t4.ftcdn.net/jpg/05/57/64/85/360_F_557648557_hyn34muXj7kwad5cOAoOjP2lRxMZOYBl.jpg" />
 
           </div>
           <img
-            className="bg-cover rounded-2xl" src="https://i.ibb.co/GtRvHYV/f22.png"/>
+            className="bg-cover sm:w-[60%] w-[98%] sm:rounded-2xl" src="https://i.ibb.co/GtRvHYV/f22.png"/>
              <div className="flex flex-col justify-center gap-10">
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://ddnews.gov.in/wp-content/uploads/2024/03/fireworks-1953253_640.jpg" />
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://www.tarotread.ai/images/posts/dream-about-firecrackers.webp" />
-            <img className="bg-cover w-28 h-28 rounded-3xl" src="https://media.istockphoto.com/id/1158137464/photo/celebration-with-fire-crackers.jpg?s=612x612&w=0&k=20&c=x2oJadNAYJUoa-dK2xAS6Ctb0GTvxacdQDrHk-uz3Fc=" />
-
+            <img className="bg-cover hidden sm:block sm:w-28 sm:h-28 rounded-3xl" src="https://ddnews.gov.in/wp-content/uploads/2024/03/fireworks-1953253_640.jpg" />
+            <img className="bg-cover hidden sm:block sm:w-28 sm:h-28 rounded-3xl" src="https://www.tarotread.ai/images/posts/dream-about-firecrackers.webp" />
+            <img className="bg-cover hidden sm:block sm:w-28 sm:h-28 rounded-3xl" src="https://media.istockphoto.com/id/1158137464/photo/celebration-with-fire-crackers.jpg?s=612x612&w=0&k=20&c=x2oJadNAYJUoa-dK2xAS6Ctb0GTvxacdQDrHk-uz3Fc=" />
           </div>
+
         </div>
 
 
-        <div className="w-full"><img className="w-full h-8" src="https://i.ibb.co/wJbwS3h/ds.jpg" /></div>
-        <div className="mt-20 text-center text-[#f08080] text-5xl" > <p>ℂ𝕖𝕝𝕖𝕤𝕥𝕚𝕒𝕝 ℂ𝕖𝕝𝕖𝕓𝕣𝕒𝕥𝕚𝕠𝕟𝕤: ℙ𝕖𝕣𝕤𝕠𝕟𝕒𝕝𝕚𝕫𝕖𝕕 𝔽𝕚𝕣𝕖𝕨𝕠𝕣𝕜𝕤 𝕄𝕒𝕘𝕚𝕔! </p></div>
-        <div className=" text-center text-[#f4978e] text-3xl"> <p>𝓔𝓷𝓬𝓱𝓪𝓷𝓽 𝔂𝓸𝓾𝓻 𝔀𝓮𝓭𝓭𝓲𝓷𝓰 𝓷𝓲𝓰𝓱𝓽. </p></div>
+        <div className="w-full"><img className="w-full sm:h-8 h-4" src="https://i.ibb.co/wJbwS3h/ds.jpg" /></div>
+        <div className="sm:mt-20 mt-4 text-center text-[#f08080] sm:p-0 text-2xl sm:text-5xl" > <p>ℂ𝕖𝕝𝕖𝕤𝕥𝕚𝕒𝕝 ℂ𝕖𝕝𝕖𝕓𝕣𝕒𝕥𝕚𝕠𝕟𝕤: ℙ𝕖𝕣𝕤𝕠𝕟𝕒𝕝𝕚𝕫𝕖𝕕 𝔽𝕚𝕣𝕖𝕨𝕠𝕣𝕜𝕤 𝕄𝕒𝕘𝕚𝕔! </p></div>
+        <div className=" text-center text-[#f4978e] text-xl mt-1 sm:text-3xl"> <p>𝓔𝓷𝓬𝓱𝓪𝓷𝓽 𝔂𝓸𝓾𝓻 𝔀𝓮𝓭𝓭𝓲𝓷𝓰 𝓷𝓲𝓰𝓱𝓽. </p></div>
        
-        <div className="flex p-32">
+        <div className="flex flex-col sm:gap-0 gap-20 sm:flex-row p-1 sm:p-32">
 
-        <div className=" flex flex-col w-[50%] gap-2">
-        <p className="text-black text-xl font-bold" data-aos="zoom-out">Key Highlights</p>
-        <p className="text-3xl w-[70%] text-[#ff5d8f]" > Memorable Wedding Fireworks by QR Weds</p>
-        <p className="text-black mt-2 w-[70%]"> We offer tailored packages, selecting elements from your vision to create an unforgettable experience that marks the beginning of a beautiful journey.</p>
-        <p className="font-semibold text-xl mt-2 text-black ml-6" data-aos="zoom-in">Spectacular Fireworks Show</p>
-        <p className="text-black w-[65%] ml-6 mt-1">Dazzling, synchronized displays designed to awe guests and capture breathtaking moments. Perfect for every couple’s big day.</p>
+        <div className=" flex flex-col sm:w-[50%] gap-2 text-balance text-center">
+        <p className="text-black text-xl sm:-ml-96   font-bold" data-aos="zoom-out">Key Highlights</p>
+        <p className="sm:text-3xl text-2xl sm:w-[70%] text-[#ff5d8f]" > Memorable Wedding Fireworks by QR Weds</p>
+        <p className="text-black mt-2 sm:w-[70%]"> We offer tailored packages, selecting elements from your vision to create an unforgettable experience that marks the beginning of a beautiful journey.</p>
+        <p className="font-semibold text-xl sm:text-left mt-2 hidden sm:block text-black ml-6" data-aos="zoom-in">Spectacular Fireworks Show</p>
+        <p className="text-black w-[65%] ml-6 hidden sm:block mt-1">Dazzling, synchronized displays designed to awe guests and capture breathtaking moments. Perfect for every couple’s big day.</p>
         </div>
 
 
-       <div className="flex flex-col w-[50%] gap-10">  
-       <div className="bg-[#f2cfc4] flex rounded-lg gap-10" data-aos="fade-down">
-       <img className="w-20 h-20 ml-[4%] pt-4" src="https://i.ibb.co/KxYRkDh/star.png" />                                                                                                                             
+       <div className="flex flex-col sm:w-[50%] gap-10">  
+       <div className="bg-[#f2cfc4] flex rounded-lg sm:gap-10 gap-2" data-aos="fade-down">
+       <img className="mt-[5%] sm:mt-0 w-20 h-20 sm:ml-[4%] pt-4" src="https://i.ibb.co/ZxvV5kT/star-removebg-preview.png" />                                                                                                                             
        <div className="flex flex-col">
-       <p className=" text-[#1282a2] text-2xl font-bold pt-2"> Traditional Indian Fireworks</p>
-       <p className="text-left font-semibold  text-black pb-2"> Experience the magic of India’s rich fireworks tradition, adding vibrant colors and awe-inspiring patterns to your celebration.</p>
+       <p className=" text-[#1282a2] text-xl sm:text-2xl font-bold pt-2"> Traditional Indian Fireworks</p>
+       <p className="text-left text-[15px] sm:font-semibold text-black pb-2"> Experience the magic of India’s rich fireworks tradition, adding vibrant colors and awe-inspiring patterns to your celebration.</p>
        </div>
        </div>
-       <div className="bg-[#f2cfc4] flex rounded-lg gap-10" data-aos="zoom-in">
-       <img className="w-20 h-20 ml-[4%] pt-4" src="https://i.ibb.co/WfSHS1W/star2.png" />  
+       <div className="bg-[#f2cfc4] flex rounded-lg sm:gap-10 gap-2" data-aos="zoom-in">
+       <img className="mt-[5%] sm:mt-0 w-20 h-20 sm:ml-[4%] pt-4" src="https://i.ibb.co/YWgPDCK/star2-removebg-preview-1.png" />  
        <div className="flex flex-col">
-       <p className=" text-[#1282a2] text-2xl font-bold" > Customized Displays</p>
-       <p className="text-left font-semibold  text-black pb-2"> Get a display designed specifically for your wedding, with customized colors, styles, and timing to match your theme perfectly</p>
+       <p className=" text-[#1282a2] text-xl sm:text-2xl font-bold pt-2" > Customized Displays</p>
+       <p className="text-left text-[15px] sm:font-semibold  text-black pb-2"> Get a display designed specifically for your wedding, with customized colors, styles, and timing to match your theme perfectly</p>
        </div>
        </div>
-       <div className="bg-[#f2cfc4] flex gap-10 rounded-lg " data-aos="fade-up">
-       <img className="w-20 h-20 ml-[4%] pt-4" src="https://i.ibb.co/KxYRkDh/star.png" />
+       <div className="bg-[#f2cfc4] flex gap-2 sm:gap-10 rounded-lg " data-aos="fade-up">
+       <img className="mt-[5%] sm:mt-0 w-20 h-20 sm:ml-[4%] pt-4" src="https://i.ibb.co/ZxvV5kT/star-removebg-preview.png" />
        <div className="flex flex-col">
-       <p className=" text-[#1282a2] text-2xl font-bold">Eco-Friendly Options </p>
-       <p className=" text-left font-semibold  text-black pb-2"> Choose eco-friendly fireworks that minimize environmental impact, ensuring a celebration that’s both stunning and sustainable.</p>
+       <p className=" text-[#1282a2] sm:text-2xl text-xl font-bold pt-2">Eco-Friendly Options </p>
+       <p className=" text-left text-[15px] sm:font-semibold  text-black pb-2"> Choose eco-friendly fireworks that minimize environmental impact, ensuring a celebration that’s both stunning and sustainable.</p>
        </div>
        </div>
        </div>
@@ -77,4 +77,4 @@ const FireCrakers = () => {
     </>
   );
 };
-export default FireCrakers;
+export default FireCrakers; 
