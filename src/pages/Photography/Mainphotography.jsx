68dbecef642bbc4f0f3d552f0494dbbd14f43982
Ibrangrd <ibrangrd@gmail.com>
2025-01-12 23:05:43 +0530

    Make chnage in service as well as in photography page

--- src/pages/Photography/Mainphotography.jsx
+++ src/pages/Photography/Mainphotography.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import "aos/dist/aos.css";
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 const Videography = (props) =>{
     return (
         
@@ -11,14 +13,16 @@ const Videography = (props) =>{
                 <div className="info">
                     <div className="na">{props.title}</div>
                     <div className="pr">{props.star}
-                    <div className="ra">{props.rating}</div>
+                    {/* <div className="ra">{props.rating}</div> */}
                     </div>
                 </div>
                 <div className="par">{props.des}</div>
                 <div className="bu">
-                <Link to={props.link}>
-                    <button className="but"> View </button>
-                    </Link>
+                <Tooltip className="text-5xl" title="VIEW">
+                <Link to={props.Link}>
+                 <Button className="w-52 pb-10 h-12 hover:bg-blue-100 hover:text-red-700" variant="outlined">VIEW</Button>
+                 </Link> 
+                 </Tooltip>
                 </div>
             </div>
             
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -8,20 +8,20 @@ return (
     <>
     <div className="text-center bg-[#ffff]">
        {/* <div className="flex flex-col"> */}
-        <p className="mt-4 text-4xl sm:text-5xl pb-10 font-bold pt-4 text-[#00b4d8] text-shadow-ib5">
+        <p className="mt-4 text-4xl sm:text-5xl pb-10 font-bold pt-4 text-[#00b4d8]">
         OUR SERVICES
         </p>
       {/* </div> */}
      
-      <div className="flex flex-col flex-wrap sm:flex-row justify-center p-2 sm:p-10 h-auto gap-10 pb-16 ">
+      <div className="flex flex-row flex-wrap justify-center p-2 sm:p-10 h-auto gap-10 pb-16 ">
       {
       Service2List.map((val,index) =>
-      <div key={index} className="mb-10 flex flex-col w-[100%] sm:w-[30%] h-[450px] justify-center text-center text-image rounded-2xl bg-[#ffff] gap-4 pt-4 pb-4 ">
+      <div key={index} className="mb-10 flex flex-col w-[90%] sm:w-[30%] h-auto justify-center text-center text-image rounded-2xl bg-[#ffff] gap-2 pt-4 pb-4 ">
       <img className="m-auto h-[70%] w-[90%] rounded-xl boxShadow text-box11" src={val.imgsrc} />
-      <h1 className="text-3xl pt-6 font-bold text-[#4f772d]">{val.heading}</h1>
+      <h1 className="text-3xl pt-2 font-bold text-[#4f772d]">{val.heading}</h1>
       <Tooltip className="text-5xl" title="VIEW">
       <Link to={val.Link}>
-      <Button className="w-52 pb-10 h-12 hover:bg-blue-100 hover:text-red-700" variant="outlined">VIEW</Button>
+      <Button className="w-52 pb-6 h-12 hover:bg-blue-100 hover:text-red-700" variant="outlined">VIEW</Button>
       </Link> 
       </Tooltip>
       </div> 
