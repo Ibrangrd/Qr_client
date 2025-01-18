a8de5704795d6d0e4c81a18b72616d9958480779
Ibrangrd <ibrangrd@gmail.com>
2025-01-18 09:39:39 +0530

    fix the problem in photography page

--- src/pages/Photography/Mainphotography.jsx
+++ src/pages/Photography/Mainphotography.jsx
@@ -19,7 +19,7 @@ const Videography = (props) =>{
                 <div className="par">{props.des}</div>
                 <div className="bu">
                 <Tooltip className="text-5xl" title="VIEW">
-                <Link to={props.Link}>
+                <Link to={props.link}>
                  <Button className="w-52 pb-10 h-12 hover:bg-blue-100 hover:text-red-700" variant="outlined">VIEW</Button>
                  </Link> 
                  </Tooltip>
