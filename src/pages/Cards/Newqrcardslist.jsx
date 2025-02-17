73de1c77df6e5a460a14ed644be300c32ca796b2
Ibrangrd <ibrangrd@gmail.com>
2025-02-17 14:03:57 +0530

    Chnage the style in the qr weds cards

--- src/pages/Cards/Newqrcardslist.jsx
+++ src/pages/Cards/Newqrcardslist.jsx
@@ -6,14 +6,14 @@ const Newqrcardslist = ({ path,title,price,des }) => {
     <div className=" w-[100%]" data-aos="flip-right">
       <div className="group  md:px-5 py-5">
         <div
-          className={`mx-1 my-5 group rounded-lg  shadow-lg hover:scale-105 hover:shadow-gray-300  duration-700 ease-in-out p-2 flex flex-col items-center `}
+          className={`mx-1 my-5 group rounded-lg shadow-lg scale-[1.16] sm:scale-110 sm:hover:scale-[1.12] hover:shadow-gray-300 duration-700 ease-in-out p-2 flex flex-col items-center `}
         >
           <img
             className="h-[15rem] sm:h-[20rem] w-11/12 rounded-lg group-hover:scale-105 duration-700 pb-2 pt-1"
             src={path}
           ></img>
           <div className="flex justify-between p-2 w-full">
-            <p className="text-lg sm:text-3xl font-bold  text-[#C75B7A]">
+            <p className="text-lg sm:text-3xl font-bold text-[#C75B7A]">
               {title}
             </p>
             <p className="text-lg sm:text-xl font-bold text-green-500">
