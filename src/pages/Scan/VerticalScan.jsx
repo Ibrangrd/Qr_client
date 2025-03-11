b0d188f6f61082aedfb6a8fa66a954921913a295
Ibrangrd <ibrangrd@gmail.com>
2025-03-11 11:42:38 +0530

    make the changes in scan page for the customer

--- src/pages/Scan/VerticalScan.jsx
+++ src/pages/Scan/VerticalScan.jsx
@@ -18,43 +18,58 @@ const VerticalScan = () => {
       <div className=" h-auto w-auto bg-cover bg-no-repeat " style={{ backgroundImage: `url("https://i.ibb.co/HHgF694/freepik.jpg")` }}>
           <div className=" flex flex-col">
             <div className=" flex justify-center text-center gap-3">
-              <h1 className=" text-2xl sm:text-[35px] font-bold mt-[60px] italic text-[#e9ff70]">
+              <h1 className=" text-[20px] sm:text-[35px] font-bold mt-[60px] italic text-[#e9ff70]">
               आयुष्मती
               </h1>
               <img
                 className=" w-20 h-20 mt-10 animate-heart-size"
                 src="https://i.postimg.cc/0QNBbMSM/testimage.png"
               />
-              <h1 className=" text-2xl sm:text-[35px] font-bold mt-[60px] italic text-[#e9ff70]">
+              <h1 className=" text-[20px] sm:text-[35px] font-bold mt-[60px] italic text-[#e9ff70]">
               आयुष्मान् 
               </h1>
             </div>
-            <div className="mt-[28%] ml-[6%] sm:flex sm:justify-center sm:items-center sm:mt-[10%]">
+            {/* <div className="mt-[28%] ml-[6%] sm:flex sm:justify-center sm:items-center sm:mt-[10%]">
               <iframe
                 className="border-double border-[#C2A5A1] border-8 w-[85%] h-[280px] sm:w-[50%] sm:h-[500px]"
                 // style="w-[85%] h-[220px]"
-                src="https://www.youtube.com/embed/JN8jhTjEckE?autoplay=1&si=jx_4fd0vRaeFIHox"
+                src="https://vimeo.com/1064439735/a251396d4d?ts=0&share=copy"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; allowFullScreen"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
               ></iframe>
-            </div>
-            <div className="pt-16 flex justify-center gap-14 mb-[14%] sm:mb-0 sm:mt-[7%] sm:gap-60 sm:ml-[30%]">
+              
+            </div> */}
+            <div className="mt-[28%] ml-[6%] sm:flex sm:justify-center sm:items-center sm:mt-[10%]">
+<iframe src="https://player.vimeo.com/video/1064439735?h=a251396d4d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+frameborder="0" 
+allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" 
+className="border-double border-[#C2A5A1] border-8 w-[85%] h-[280px] sm:w-[50%] sm:h-[500px]"
+ title=".">
+
+ </iframe>
+ </div>
+ <script src="https://player.vimeo.com/api/player.js"></script>
+            <div className="pt-6 pb-8 flex justify-center gap-14 mb-[14%] sm:mb-0 sm:mt-[7%] sm:gap-60 sm:ml-[30%]">
               <div className="">
                 <div className="text-[15px] font-bold  text-green-800 text-center">
-        
+                <a
+                    href="https://maps.app.goo.gl/S2SHZcypcHbMHS86A"
+                    target="_blank"
+                  >
                   Wedding Venue
+                  </a>
                 </div>
-                <div className="ml-[28%]">
+                <div className="ml-[12%]">
                   <a
                     href="https://maps.app.goo.gl/S2SHZcypcHbMHS86A"
                     target="_blank"
                   >
                     <img
-                      className="w-10 h-10"
-                      src="https://iili.io/dL52914.png"
+                      className="w-16 h-16"
+                      src="https://i.ibb.co/27mz9vpK/Whats-App-Image-2025-03-11-at-00-37-42-db78e4e4-removebg-preview.png"
                     />
                   </a>
                 </div>
@@ -62,16 +77,21 @@ const VerticalScan = () => {
               <div className="">
                 <div className="text-[15px] font-bold text-green-800 text-center">
                   {" "}
+                  <a
+                    href="https://maps.app.goo.gl/aU523jurYw5ZsYxJ6"
+                    target="_blank"
+                  >
                   Residence{" "}
+                  </a>
                 </div>
-                <div className="ml-[22%]">
+                <div className="ml-[4%]">
                   <a
                     href="https://maps.app.goo.gl/aU523jurYw5ZsYxJ6"
                     target="_blank"
                   >
                     <img
-                      className="w-10 h-10"
-                      src="https://iili.io/dL52914.png"
+                      className="w-16 h-16"
+                      src="https://i.ibb.co/27mz9vpK/Whats-App-Image-2025-03-11-at-00-37-42-db78e4e4-removebg-preview.png"
                     />
                   </a>
                 </div>
